3f3a65e3486e4fc08f12ee795c8ae572432396a3
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2026-08-27 19:39:35 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.codecrafter.applock"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.codecrafter.applock"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
